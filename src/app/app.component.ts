3135012ae1a2eb01694ebd3334779348511fe5e8
Rrahel Cemi <rrahel@debian.home>
2017-06-30 00:24:08 +0200

    notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,7 +41,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
-      this.notify.createNotifications();
+      this.notify.getLocation();
     });
   }
 
--- src/app/notifications.ts
+++ src/app/notifications.ts
@@ -1,54 +1,109 @@
-import { AlertController} from 'ionic-angular';
-import {LocalNotifications} from '@ionic-native/local-notifications';
-import {Platform} from 'ionic-angular';
-import {Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import {
+  AlertController
+} from 'ionic-angular';
+import {
+  LocalNotifications
+} from '@ionic-native/local-notifications';
+import {
+  Platform
+} from 'ionic-angular';
+import {
+  Injectable
+} from '@angular/core';
+import {
+  Http
+} from '@angular/http';
+import * as moment from 'moment';
+import 'rxjs/add/operator/map';
+import {
+  Geolocation
+} from '@ionic-native/geolocation';
 
 
-const API_URL = 'https://holiday-checker.herokuapp.com/api/';
 
+const API_URL = 'https://holiday-checker.herokuapp.com/api';
+const GEOCODING_URL = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=';
 
 @Injectable()
 export class Notifications {
-    private posts;
-    constructor(private alertCtrl: AlertController, private localNotifications: LocalNotifications, private plt: Platform, public http: Http) {        
+  private posts = [];
+  private eventJSON = [];
+  private location = '';
 
-    }
+  constructor(private platform: Platform, private geolocation: Geolocation, private alertCtrl: AlertController, private localNotifications: LocalNotifications, private plt: Platform, public http: Http) {
 
-    createNotifications(){
-        let now = new Date();
-        this.http.get(API_URL + location + '/' + now + '/' + new Date(now + '1*30*24*60*60*1000'))
-        .map(res => res.json()).subscribe(data => {
-            this.posts = data;            
-            let alert = this.alertCtrl.create({
-                title: this.posts.englishName,
-                subTitle: this.posts
-            });
-            alert.present();
+  }
+
+  public createNotifications() {
+    let now = moment(new Date()).format('YYYY-MM-DD');
+    let endDate = moment(now).add('days', 5).format('YYYY-MM-DD');
+    let time = new Date(new Date().getTime() + 5 * 1000);
+    let index = 0;
+
+    console.log(API_URL + '/' + this.location + '/' + now + '/' + endDate);
+    this.http.get(API_URL + '/' + this.location + '/' + now + '/' + endDate)
+      .map(res => res.json()).subscribe(data => {
+        data.forEach(element => {
+
+          this.localNotifications.schedule({
+            id: index,
+            title: 'Holiday',
+            text: element.englishName,
+            data: {
+              eventCountry: element.country,
+              eventTitle: element.englishName,
+              eventDate: moment(element.isoDate).format('DD.MM.YYYY')
+            },
+            at: time
+          });
+          index++;
         });
+      });
 
-        // this.localNotifications.cancelAll();
+    this.localNotifications.on('click', (notification, state) => {
+      let json = JSON.parse(notification.data);
+      console.log(json);
+      let alert = this.alertCtrl.create({
+        title: 'Holiday Checker',
+        subTitle: json.eventTitle,
+        message: json.eventCountry + '<br>' + json.eventDate
+      });
+      alert.present();
+    })
+  }
 
-        // this.localNotifications.schedule({
-        //     id: 1,
-        //     title: 'Attention',
-        //     text: 'Notification',
-        //     data: { mydata: 'My hidden message this is' },
-        //     at: new Date(new Date().getTime() + 10 * 1000)            
-        // });
-        
-        
-        // this.localNotifications.on('click', (notification, state) => {
-        //     let json = JSON.parse(notification.data);
-        
-        //     let alert = this.alertCtrl.create({
-        //         title: notification.title,
-        //         subTitle: json.mydata
-        //     });
-        //     alert.present();
-        // })
 
-        
-    }
+
+  public getLocation() {
+    let lat, lng;
+
+
+      this.geolocation.getCurrentPosition().then((resp) => {
+
+        lat = resp.coords.latitude;
+        lng = resp.coords.longitude;
+
+        //return this.http.get(GEOCODING_URL + lat + '%2C' + lng + '&language=en')
+        this.http.get(`${GEOCODING_URL}${lat}%2C${lng}&language=en`)
+          .map(res => res.json())
+          .subscribe((data: any) => {
+
+            if (data.results !== undefined && data.results !== null) {
+              data.results.forEach(res => {
+                res.address_components.forEach(adress => {
+                  if (adress.types[0] === 'country') {
+                    this.location = adress.long_name;                    
+                  }
+                })
+              })
+            }
+
+            if(this.location != '') {
+                this.createNotifications();
+            }
+          });
+
+      })
+  }
 
 }
